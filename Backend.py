fbd43e1304a14ed9bfde82e3618e4e2526f1810e
unknown <vudtipat@gmail.com>
2020-12-15 16:33:19 +0700

    message

--- Backend.py
+++ Backend.py
@@ -1,98 +1,155 @@
-from flask import Flask,request,json,Response
-import psycopg2
-from psycopg2.extras import RealDictCursor
+from flask import Flask,request,json
+import pandas as pd 
+from gensim.models import Word2Vec
+from pythainlp.tokenize import word_tokenize
+import pythainlp.corpus as st
+import numpy as np
+import pymongo
 
-       
-def insert(table,data):
-    try:
-        connection = psycopg2.connect(user = "ppociznxzknvfz",
-                                      password = "e7e5d5872f97e341660c93d04ea2682a3d9f892c450b1ec8c5679903e6387162",
-                                      host = "ec2-34-234-185-150.compute-1.amazonaws.com",
-                                      port = "5432",
-                                      database = "d8965kg37dlmdu")
-        cursor = connection.cursor()
-        cursor.execute('''INSERT INTO '''+table+''' (username, password) VALUES ('''+"'"+data['user']+"'"+','+"'"+data['pass']+"'"+')')
-        connection.commit()
-        connection.close()
-        print("PostgreSQL connection is closed")
-        return "SUCCESS"
-    except (Exception, psycopg2.Error) as error :
-        print ("Error while connecting to PostgreSQL", error)
-        
-def select_all(table,data):
-    try:
-        connection = psycopg2.connect(user = "ppociznxzknvfz",
-                                      password = "e7e5d5872f97e341660c93d04ea2682a3d9f892c450b1ec8c5679903e6387162",
-                                      host = "ec2-34-234-185-150.compute-1.amazonaws.com",
-                                      port = "5432",
-                                      database = "d8965kg37dlmdu")
-        cursor = connection.cursor(cursor_factory= RealDictCursor)
-        cursor.execute('select * from '+table+' where username = '+"'"+data['user']+"'")
-        result = json.dumps(cursor.fetchone())
-        connection.close()
-        print("PostgreSQL connection is closed")
-        return result
-    except (Exception, psycopg2.Error) as error :
-        print ("Error while connecting to PostgreSQL", error)
+client = pymongo.MongoClient("mongodb+srv://tum123456:ttt123456@cluster0.cfjjb.mongodb.net/<dbname>?retryWrites=true&w=majority")
+db = client.test
+mydb = db["MyProject"]
 
-def update(table,data):
-    
-    try:
-        connection = psycopg2.connect(user = "ppociznxzknvfz",
-                                      password = "e7e5d5872f97e341660c93d04ea2682a3d9f892c450b1ec8c5679903e6387162",
-                                      host = "ec2-34-234-185-150.compute-1.amazonaws.com",
-                                      port = "5432",
-                                      database = "d8965kg37dlmdu")
-        cursor = connection.cursor()
-        cursor.execute('update '+table+' set email = '+"'"+data['user']+"'"+','+' password = '+ "'"+data['pass']+"'"+ ' where username = '+"'"+data['user']+"'")
-        connection.commit()
-        connection.close()
-        print("PostgreSQL connection is closed")
-        return "SUCCESS"
-    except (Exception, psycopg2.Error) as error :
-        print ("Error while connecting to PostgreSQL", error)
-        
+
+words = st.thai_stopwords()
+data = pd.read_excel("Book1.xlsx",index_col=0) 
+word_not_important = ['หา','รับ','งาน','ทำหน้าที่','หน้าที่','ซ่อม','(',')','/']
 app = Flask(__name__)
+
+def lower(x):
+    return x.lower()
+
+def concerned_sentence(search,allsentence):
+    arr = []
+    for i in search:
+        for j in allsentence:
+            if(i.lower() in j):
+                arr.append(j)
+    return arr
+
+def clear_space(s):
+    s = s.lower()
+    global words,word_not_important
+    ss = ''
+    for i in s:
+        if(i != '/'):
+            if(i != ' '):
+                ss+=i
+    b = word_tokenize(ss)
+    bb = []
+    for i in b:
+        if(i ==  ' '):
+            None
+        elif(i in words):
+            None
+        elif(i in word_not_important):
+            None
+        else:
+            bb.append(i)
+    return bb
+
+data['Title'] = data['Title'].apply(lower)
+all_work = [clear_space(i) for i in data['Title'].tolist()]
 
 @app.route('/')
 def home():
     print("Hello World")
     return "This is home page "
 
-
-@app.route('/api/tests', methods=['GET','POST'])
-def tests():
+@app.route('/login_employee', methods=['GET','POST'])
+def login_employee():
     if(request.method == 'GET'):
-        list_x = [{
-          "name": "JohnSon",
-          "age": 30,
-          "city": "New York"
-        },{
-          "name": "JOOO",
-          "age": 30,
-          "city": "New York"
-        },{
-          "name": "BOBY",
-          "age": 30,
-          "city": "New York"
-        },{
-          "name": "TERWA",
-          "age": 30,
-          "city": "New York"
-        }]
-        response = app.response_class(
-        response=json.dumps(list_x),
-        mimetype='application/json'
-        )
+        print(123)
+        param1 = request.args.get('name')
+        param2 = request.args.get('pass')
+        print(param1+' '+param2)
+        response = {
+             'response' : 'locked in',
+             'mimetype' : 'application/json'
+                
+        }
         return response
     
-    if(request.method == 'POST'):
-        print("Success11111!!")
-        param = request.json
+@app.route('/search_job', methods=['GET','POST'])
+def search_job():
+    if(request.method == 'GET'):
+        param = request.args.get('search')
         print(param)
-        response1 = insert('accounts',param)
-        return Response(response=response1, status=200, mimetype="application/json") #return json string
+        search_word = param
+        search = clear_space(search_word)
+        select_sentence = concerned_sentence(search,all_work)
+        if(len(select_sentence) > 0):
+            model = Word2Vec(select_sentence, min_count=1,size = 500)
+            a = []
+            try:
+                for t in search:
+                    aa=model.most_similar(t)
+                    list_of_word = [i[0] for i in aa]
+                    a += list_of_word
+                    
+                    a = np.array(a) 
+                    a = np.unique(a)
+                    
+                    print("===== RESULT =====")
+                    print(a)
+                    print("===== RESULT =====")
+                    
+                    result = []
+                    for i in a:
+                        for j in range(len(all_work)):
+                            if(i in all_work[j]):
+                                result.append(j)
+                                
+                    result = np.array(result)
+                    result = np.unique(result)
+                    print("===== SEARCH RESULT =====")
+                    print(result)
+                    lst = []
+                    for z in result:
+                        da = [
+                                data.iloc[z]['Title'],
+                                data.iloc[z]['Company'],
+                                data.iloc[z]['Location'],
+                                data.iloc[z]['Property']
+                        ]
+                        lst.append(da)
+                        #print(data.iloc[z])
+                    #lst = json.dumps(lst ,ensure_ascii=False)
+                    print(lst)
+                    response = {
+                         'response' : lst,
+                         'mimetype' : 'application/json'
+                    }
+                    print("===== SEARCH RESULT =====")
+            except:
+                b = select_sentence
+                
+        else:
+            response = {
+             'response' : 'ไม่มีงานอยุ่ในระบบ',
+             'mimetype' : 'application/json'
+            }
         
+        return response
+
+
+@app.route('/Employee_Register', methods=['GET','POST'])
+def Employee_Register():
+    if(request.method == 'POST'):
+        print(request.json)
+        try:
+            mycol = mydb["Employee_Account"]
+            mycol.insert_one(request.json)
+            response = {
+                 'response' : 'Pass',
+                 'mimetype' : 'application/json'     
+            }
+        except:
+            response = {
+                 'response' : 'Cannot',
+                 'mimetype' : 'application/json'     
+            }
+        return response
 @app.route('/api/param', methods=['GET','POST'])
 def param():
     if(request.method == 'GET'):
@@ -100,10 +157,12 @@ def param():
         param2 = request.args.get('pass')
         data = {'user':param1,'pass':param2}
         print(param1+' '+param2)
-        response = app.response_class(
-                response=select_all('accounts',data),
-                mimetype='application/json'
-        )
+        response = {
+             'response' : 'locked in',
+             'mimetype' : 'application/json'
+                
+        }
+   
         return response
     
     if(request.method == 'POST'):
@@ -112,7 +171,7 @@ def param():
         data = {'user':param1,'pass':param2}
         print(param1+' '+param2)
         response = app.response_class(
-                response=update('accounts',data),
+                #response=update('accounts',data),
                 mimetype='application/json'
         )
         return response
